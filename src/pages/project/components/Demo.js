73da78be3bbceabac511c8b21ac7ede41311eaaa
Nathaly Toledo <nathaly12toledo@gmail.com>
2025-08-11 18:01:05 -0400

    feat: complete demo section
    Added the section that has the media links of each project.

--- src/pages/project/components/Demo.js
+++ src/pages/project/components/Demo.js
@@ -1,13 +1,40 @@
+ import React from 'react';
+ import './Demo.css';
+
 const DemoSection = () => {
-    return (
-        <section className="section-header top-section">
-            <div className="content-container-header content-theme-dark">
-                <div className="content-inner-header">
-                    <h1>Demo Section</h1>
-                </div>
-            </div>
-        </section>
-    );
-};
+
+   const socialMediaPills = [
+        { name: 'GitHub', url: '#', icon: 'bi bi-github'},
+        { name: 'Demo video', url: '#', icon: 'bi bi-camera-reels-fill'},
+        { name: 'Live demo', url: '#', icon: 'bi bi-laptop-fill' },
+        { name: 'Marketplace', url: '#', icon: 'bi bi-bag-dash-fill' },
+   ]
+ 
+   return (
+     <header className="demo-header d-flex flex-column">
+ 
+       {/* Main Content */}
+       <div className="container flex-grow-1 d-flex flex-column justify-content-center text-center text-md-start">
+         <h1 className="display-2 fw-bold mb-4 gradient-text text-center">
+           Code + Live Previews
+         </h1>
+ 
+         {/* Media Links */}
+         <div className="tech-stack d-flex flex-column gap-3 mt-3">
+           <div className="d-flex flex-wrap gap-5 justify-content-center">
+           
+                {socialMediaPills.map((pill, i) => (
+                    <a key={i} href={pill.url} className="btn btn-outline-light tech-pill">
+                        <i className={pill.icon}></i> {pill.name}
+                    </a>
+                ))}
+           </div>
+         </div>
+       </div>
+     </header>
+   );
+ };
+
+ 
 
 export default DemoSection;
